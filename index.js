78267a6c36c3e29a4c0cbd68a7ae3e12a2cd02d4
dnhn-design <donghoon.yi.portfolio@gmail.com>
2025-02-01 02:11:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const elements = document.querySelectorAll('.hover-effect');
+
+elements.forEach((element) => {
+  // Add touchstart event listener
+  element.addEventListener('touchstart', () => {
+    element.classList.add('touch-hover-effect');
+  });
+
+  element.addEventListener('touchend', () => {
+    // Remove touch effect
+    element.classList.remove('touch-hover-effect');
+  });
+});
+
 let navbar = document.getElementById(navbar);
 let shouldStickPos = navbar.offsetTop;
 

fc16525aeca5903f44bd8712476e58b32c59f557
dnhn-design <donghoon.yi.portfolio@gmail.com>
2025-03-05 22:53:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,22 @@ elements.forEach((element) => {
   });
 });
 
-let navbar = document.getElementById(navbar);
-let shouldStickPos = navbar.offsetTop;
+// let navbar = document.getElementById(navbar);
+// let shouldStickPos = navbar.offsetTop;
 
-function addOrRemoveStickyClass() {
-    if(window.scrollY >= shouldStickPos) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    };
-}
+// function addOrRemoveStickyClass() {
+//     if(window.scrollY >= shouldStickPos) {
+//         navbar.classList.add("sticky");
+//     } else {
+//         navbar.classList.remove("sticky");
+//     };
+// }
 
-window.onscroll = () => {
-    addOrRemoveStickyClass();
-}
+// window.onscroll = () => {
+//     addOrRemoveStickyClass();
+// }
 
-window.onresize = () => {
-    shouldStickPos = navbar.offsetTop;
-}
+// window.onresize = () => {
+//     shouldStickPos = navbar.offsetTop;
+// }
+
